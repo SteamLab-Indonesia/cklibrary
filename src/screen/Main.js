7ac4c42928ec55de964711933c37042564d9d0bc
hs <hs@hss-MacBook-Pro.local>
2019-09-08 16:31:40 +0700

    Full width main

--- src/screen/Main.js
+++ src/screen/Main.js
@@ -2,12 +2,18 @@ import React, {Component} from 'react';
 import Header from '../component/Header';
 import Navigation from '../component/Navigation';
 
+const styles = {
+    main: {
+        width: '100%',
+        height: '100%',
+    }
+}
 class Main extends Component {
     render() {
         return (
             <div>
                 <Header></Header>
-                <main>
+                <main style={styles.main}>
                     <Navigation />
                 </main>
             </div>
